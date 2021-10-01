c05c0c73685157f569e29b19ea5231116c6654a8
Muhamad Rizki Mardiansyah <muhamadrizkimardiansyah137@gmail.com>
2021-10-01 14:33:19 +0700

    Jurnal02

---
+++ Jurnal02.java
@@ -0,0 +1,9 @@
+public class Jurnal02 {
+
+	public static void main(String[] args) {
+		System.out.println("NIM : 6706213145");
+		System.out.println("Nama : Muhamad Rizki Mardiansyah");
+		System.out.println("Kelas : D3IF-45-04");
+		
+	}
+}
